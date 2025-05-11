45d495c32f223a664c138214a37a4448841eb7cf
Ditzyystore <mayasbintang14@gmail.com>
2025-05-12 02:50:36 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: "wdwangsaf",
+  version: "4.1.0",
+  number: "",
+  owner: {
+    name: "wdwangsaf",
+    number: "628889783468",
+    whatsapp: "628889783468@s.whatsapp.net",
+
+  },
+  author: {
+    name: "wdwangsaf",
+    number: "628889783468",
+    whatsapp: "628889783468@s.whatsapp.net",
+ 
+  },
+  features: {
+    antiCall: {
+      status: true,
+      block: false,
+    },
+    selfMode: true,
+    broadcast: {
+      text: "",
+      limit: 9999,
+      jeda: 5000,
+      filterContact: false,
+    },
+    pushContacts: {
+      text: "",
+      limit: 9999,
+      jeda: 5000,
+      filterContacts: false,
+    },
+  },
+};
